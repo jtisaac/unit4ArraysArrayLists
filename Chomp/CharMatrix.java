9b21e20c245bfc8c5827f7ae76c97b6ec6e96e68
jtisaac <jtisaac@NN123-HA-03.HS.sd203.org>
2015-12-07 15:07:18 -0600

    Arrays and chomp

--- Chomp/CharMatrix.java
+++ Chomp/CharMatrix.java
@@ -2,121 +2,118 @@
 
 public class CharMatrix
 {
-  // Instance variables:
- private char[][] grid;
-  // Constructor: creates a grid with dimensions rows, cols,
-  // and fills it with spaces
-  public CharMatrix(int rows, int cols)
-  {
-    this.grid = new char[rows][columns];
-    for (int i = 0; i< this.grid.length;i++)
+    // Instance variables:
+    private char[][] grid;
+    // Constructor: creates a grid with dimensions rows, cols,
+    // and fills it with spaces
+    public CharMatrix(int rows, int cols)
     {
-        for (int j= 0; j< this.grid[i].length; j++)
+        this.grid = new char[rows][cols];
+        for (int i = 0; i< this.grid.length;i++)
         {
-            this.grid[i][j] = ' ';
+            for (int j= 0; j< this.grid[i].length; j++)
+            {
+                this.grid[i][j] = ' ';
+            }
         }
     }
-}
 
-  // Constructor: creates a grid with dimensions rows , cols ,
-  // and fills it with the fill  character
-  public CharMatrix(int rows, int cols, char fill)
-  {
-    this.grid = new char[rows][cols];
-    for (int i = 0; i< this.grid.length;i++)
+    // Constructor: creates a grid with dimensions rows , cols ,
+    // and fills it with the fill  character
+    public CharMatrix(int rows, int cols, char fill)
     {
-        for (int j= 0; j< this.grid[i].length; j++)
+        this.grid = new char[rows][cols];
+        for (int i = 0; i< this.grid.length;i++)
         {
-            this.grid[i][j] = fill;
+            for (int j= 0; j< this.grid[i].length; j++)
+            {
+                this.grid[i][j] = fill;
+            }
         }
     }
-  }
 
-  // Returns the number of rows in grid
-  public int numRows()
-  {
-    return rows;           
-  }
-
-  // Returns the number of columns in grid
-  public int numCols()
-  {
-    return cols;
-  }
-
-  // Returns the character at row, col location
-  public char charAt(int row, int col)
-  {
-    return grid[row][col];
-  }
-
-  // Sets the character at row, col location to ch
-  public void setCharAt(int row, int col, char ch)
-  {
-    ch = grid[row][col];
-    return grid;
-  }
-
-  // Returns true if the character at row, col is a space,
-  // false otherwise
-  public boolean isEmpty(int row, int col)
-  {
-    char cell = this.grid[row][col];
-    if (cell == ' ')
+    // Returns the number of rows in grid
+    public int numRows(int rows)
     {
-        boolean empty = true;
+        return rows;           
     }
-    else
-    {
-        empty = false;
-    }
-   return empty;
-  }
 
-  // Fills the given rectangle with fill  characters.
-  // row0, col0 is the upper left corner and row1, col1 is the
-  // lower right corner of the rectangle.
-  public void fillRect(int row0, int col0, int row1, int col1, char fill)
-  { this.grid = new char[row1 + 1] [col1 + 1];
-    for (int i = 0; i< row1 + 1; i++)
+    // Returns the number of columns in grid
+    public int numCols(int cols)
     {
-        for (int j= 0; j< col1 + 1; j++)
+        return cols;
+    }
+
+    // Returns the character at row, col location
+    public char charAt(int row, int col)
+    {
+        return grid[row][col];
+    }
+
+    // Sets the character at row, col location to ch
+    public void setCharAt(int row, int col, char ch)
+    {
+        ch = grid[row][col];
+        //return this.grid;
+    }
+
+    // Returns true if the character at row, col is a space,
+    // false otherwise
+    public boolean isEmpty(int row, int col)
+    {
+        char cell = this.grid[row][col];
+        boolean empty;
+        if (cell == ' ')
         {
-            this.grid[i][j] = fill;
+            empty = true;
+        }
+        else
+        {
+            empty = false;
+        }
+        return empty;
+    }
+
+    // Fills the given rectangle with fill  characters.
+    // row0, col0 is the upper left corner and row1, col1 is the
+    // lower right corner of the rectangle.
+    public void fillRect(int row0, int col0, int row1, int col1, char fill)
+    { this.grid = new char[row1 + 1] [col1 + 1];
+        for (int i = 0; i<= row1 + 1; i++)
+        {
+            for (int j= 0; j<= col1 + 1; j++)
+            {
+                this.grid[i][j] = fill;
+            }
+        }
+
+    }
+
+    // Fills the given rectangle with SPACE characters.
+    // row0, col0 is the upper left corner and row1, col1 is the
+    // lower right corner of the rectangle.
+    public void clearRect(int row0, int col0, int row1, int col1)
+    {
+        this.grid = new char[row1 + 1] [col1 + 1];
+        for (int i = 0; i< row1 + 1; i++)
+        {
+            for (int j= 0; j< col1 + 1; j++)
+            {
+                this.grid[i][j] = ' ';
+            }
         }
     }
-    
-  }
 
-  // Fills the given rectangle with SPACE characters.
-  // row0, col0 is the upper left corner and row1, col1 is the
-  // lower right corner of the rectangle.
-  public void clearRect(int row0, int col0, int row1, int col1)
-  {
-    this.grid = new char[row1 + 1] [col1 + 1];
-    for (int i = 0; i< row1 + 1; i++)
+    // Returns the count of all non-space characters in row 
+    public int countInRow(int row)
     {
-        for (int j= 0; j< col1 + 1; j++)
+        int count = 0;
+        for(int j = 0; j < this.grid[row].length; j++)
         {
-            this.grid[i][j] = ' ';
+            count += this.grid[row] [j];
         }
+        
+        return count;
     }
-  }
 
-  // Returns the count of all non-space characters in row 
-  public int countInRow(int row)
-  {
-    for ( value : this.grid}
-    {
-    System.out.println("ksjdksjdf");
-}
-   
-    
-  
-  }
-  // Returns the count of all non-space characters in col 
-  public int countInCol(int col)
-  {
-    ...
-  }
 }
